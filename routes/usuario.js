ab3570f0feaed2c64326af4900b6cde825698e77
[Mario Nuñez] <[mario.nunezf@gmail.com]>
2020-06-19 13:55:43 -0400

    activacion

--- routes/usuario.js
+++ routes/usuario.js
@@ -126,6 +126,60 @@ app.post('/', (req, res) => {
 //         })
 //     });
 // });
+//===================================
+// Activar usuario
+//==================================
+app.put('/activar/:id', (req, res) => {
+
+    var id = req.params.id;
+    // var body = req.body;
+
+    Usuario.findById(id, (err, usuario) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar Usuario',
+                errors: err
+            });
+        }
+
+        if (!usuario) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'El usuario con el id ' + id + ' no existe',
+                errors: { mensaje: 'No existe usuario' }
+            });
+        }
+
+        if (usuario.estado == 'registrado') {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'tu cuenta ya se encuenta activa',
+                // errors: { mensaje: 'No existe usuario' }
+            });
+        }
+        // usuario.nombre = body.nombre;
+        // usuario.email = body.email;
+        // usuario.role = body.role;
+        usuario.estado = 'registrado'
+
+        usuario.save((err, usuarioGuardado) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar Usuario',
+                    errors: err
+                });
+            }
+            usuario.password = ':)'
+            res.status(200).json({
+                ok: true,
+                body: usuarioGuardado
+            });
+        })
+    });
+});
 
 
 // //===================================
